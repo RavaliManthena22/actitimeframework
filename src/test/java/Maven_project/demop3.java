5b32241774473babf3109c35d19b078fb810ced2
User <User@DESKTOP-M3CHQ9U>
2023-04-25 15:58:09 +0530

    k

---
+++ src/test/java/Maven_project/demop3.java
@@ -0,0 +1,13 @@
+package Maven_project;
+
+import org.testng.annotations.Test;
+
+import GenericUtility.Baseclass;
+
+public class demop3 extends Baseclass{
+@Test
+	public void test2()
+	{
+		System.out.println("demo");
+	}
+}
