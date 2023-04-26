76d0b82ef24e16ee4e062e9914f4f1cb2f7e2c56
User <User@DESKTOP-M3CHQ9U>
2023-04-26 15:19:38 +0530

    fg

--- src/test/java/Maven_project/demop3.java
+++ src/test/java/Maven_project/demop3.java
@@ -8,12 +8,11 @@ public class demop3 extends Baseclass{
 @Test
 	public void test2()
 	{
-<<<<<<< HEAD
+
 		System.out.println("happy");
 		System.out.println("vibes");
-=======
+
 		System.out.println("Frustration bcz of rahtfdj");
->>>>>>> branch 'master' of https://github.com/RavaliManthena22/framework.git
 	}
 public void test()
 {
